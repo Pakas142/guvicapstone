757df4963da6d0505ca50c516ee747045dbd29c2
root <root@ip-172-31-23-250.ec2.internal>
2023-04-26 18:15:08 +0000

    last commit

--- build.sh
+++ build.sh
@@ -12,6 +12,5 @@ elif [ "$GIT_BRANCH" = "origin/master" ]; then
     docker login -u pakas142 -p Pakas@142
     docker tag $IMAGE_NAME pakas142/prod:latest
     docker push pakas142/prod:latest
-docker ps
 else echo "either git pull not available in dev or master "
 fi
