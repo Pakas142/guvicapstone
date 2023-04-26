4cb1cb25fb669ddcab13a5d6071014da0c08d482
Pakas142 <120348717+Pakas142@users.noreply.github.com>
2023-04-26 19:10:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    const url = "http://localhost:5000/users";
+    const url = "http://54.175.67.2/users";
     fetch(url)
       .then((response) => response.json())
       .then((result) => {
