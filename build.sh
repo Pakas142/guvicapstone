c61dd91fcfbe88288a850a141c846512b4a78507
Pakas142 <120348717+Pakas142@users.noreply.github.com>
2023-04-26 18:21:57 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,8 +5,6 @@ if [ "$GIT_BRANCH" = "origin/dev" ]; then
     docker login -u pakas142 -p Pakas@142
     docker tag $IMAGE_NAME pakas142/dev:latest
     docker push pakas142/dev:latest
-	sed 's|{{IMAGE_NAME}}|${IMAGE_NAME}|' docker-compose.template.yml > docker-compose.yml
-    docker ps
 elif [ "$GIT_BRANCH" = "origin/master" ]; then
 	docker stop $(docker ps -aq)
     docker image prune -af
@@ -14,7 +12,5 @@ elif [ "$GIT_BRANCH" = "origin/master" ]; then
     docker login -u pakas142 -p Pakas@142
     docker tag $IMAGE_NAME pakas142/prod:latest
     docker push pakas142/prod:latest
-	sed 's|{{IMAGE_NAME}}|${IMAGE_NAME}|' docker-compose.template.yml > docker-compose.yml
-    docker ps
 else echo "either git pull not available in dev or master"
 fi
